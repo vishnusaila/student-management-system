6993c69f5cb2b738579d28f798ee3135145dd8c9
unknown <sailavishnu12@gmail.com>
2025-08-08 12:59:01 +0530

    change chapcta

--- main_app/views.py
+++ main_app/views.py
@@ -29,24 +29,7 @@ def doLogin(request, **kwargs):
     if request.method != 'POST':
         return HttpResponse("<h4>Denied</h4>")
     else:
-        #Google recaptcha
-        captcha_token = request.POST.get('g-recaptcha-response')
-        captcha_url = "https://www.google.com/recaptcha/api/siteverify"
-        captcha_key = "6LfswtgZAAAAABX9gbLqe-d97qE2g1JP8oUYritJ"
-        data = {
-            'secret': captcha_key,
-            'response': captcha_token
-        }
-        # Make request
-        try:
-            captcha_server = requests.post(url=captcha_url, data=data)
-            response = json.loads(captcha_server.text)
-            if response['success'] == False:
-                messages.error(request, 'Invalid Captcha. Try Again')
-                return redirect('/')
-        except:
-            messages.error(request, 'Captcha could not be verified. Try Again')
-            return redirect('/')
+    
         
         #Authenticate
         user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
@@ -131,14 +114,3 @@ messaging.setBackgroundMessageHandler(function (payload) {
 
 
 
-import requests
-
-def verify_recaptcha(request):
-    recaptcha_response = request.POST.get('g-recaptcha-response')
-    data = {
-        'secret': settings.RECAPTCHA_SECRET_KEY,
-        'response': recaptcha_response
-    }
-    r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
-    result = r.json()
-    return result.get('success')
