b373fd16308ec33b6516bf2c5a56b1da4152ab19
unknown <sailavishnu12@gmail.com>
2025-08-08 14:19:20 +0530

    rama

--- main_app/EmailBackend.py
+++ main_app/EmailBackend.py
@@ -1,16 +1,17 @@
-
-
-
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
-UserModel = get_user_model()
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
+    def authenticate(self, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
         try:
             user = UserModel.objects.get(email=username)
-            if user.check_password(password):
-                return user
         except UserModel.DoesNotExist:
             return None
+        else:
+            if user.check_password(password):
+                return user
+        return None
+
+
